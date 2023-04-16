2733440bb319e351da92f2ea72df6f2df6b87b14
FernandoCelmer <email@fernandocelmer.com>
2023-04-16 04:35:27 -0300

    ⚙️ FEATURE: Update Settings

--- application/core/settings/development.py
+++ application/core/settings/development.py
@@ -29,6 +29,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'modules.account',
+    'django_bootstrap5',
 ]
 
 MIDDLEWARE = [
--- application/core/settings/production.py
+++ application/core/settings/production.py
@@ -30,6 +30,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'modules.account',
+    'django_bootstrap5',
 ]
 
 MIDDLEWARE = [
